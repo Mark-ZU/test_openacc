54fcb5af91eabb8620c5f450fca6bfcabf59ee38
Mark <mark_zjunlict@zju.edu.cn>
2022-10-26 01:32:33 +0800

    add multi times time test

--- mul.cpp
+++ mul.cpp
@@ -28,16 +28,19 @@ float r[N][N],a[N][N],b[N][N];
 
 int main(){
     std::srand(static_cast <unsigned> (std::time(0)));
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            a[i][j] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-            b[i][j] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    for(int k=0;k<4;k++){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                a[i][j] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+                b[i][j] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+            }
         }
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    
+        matrix_multiply(r,a,b);
+
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        std::cout << "Time difference " << k << "th = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
     }
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    matrix_multiply(r,a,b);
-
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 }
